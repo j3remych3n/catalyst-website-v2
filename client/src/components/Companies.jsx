57e7eb1390e3538816142f653013b95ac98c7998
Ben Hubsch <bah37@duke.edu>
2018-10-16 15:09:07 -0400

    Centering images within slide.

--- client/src/components/Companies.jsx
+++ client/src/components/Companies.jsx
@@ -15,24 +15,21 @@ const carouselSettings = {
   autoplay: true,
   speed: 4000,
   dots: false,
-  autoplaySpeed: 0,
+  autoplaySpeed: 500,
   cssEase: 'linear',
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
+        slidesToScroll: 1,
       },
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 2,
+        slidesToScroll: 1,
       },
     },
     {
