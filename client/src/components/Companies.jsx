ce295334e48e811043351d84ff7a816298938ad8
Ben Hubsch <bah37@duke.edu>
2018-10-16 14:56:41 -0400

    Tooled around with image responsivity; seem to have it figured out.

--- client/src/components/Companies.jsx
+++ client/src/components/Companies.jsx
@@ -8,16 +8,41 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const carouselSettings = {
+  pauseOnHover: false,
   infinite: true,
-  slidesToShow: 3,
+  slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
   speed: 4000,
+  dots: false,
   autoplaySpeed: 0,
   cssEase: 'linear',
-  arrows: true,
-  variableWidth: true,
-  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 
 export default class Companies extends Component {
@@ -35,81 +60,15 @@ export default class Companies extends Component {
   }
 
   render() {
-    // const { logos } = this.state;
-    // return (
-    //   <Slider {...carouselSettings}>
-    //     {logos.map(logo => (
-    //       <div key={uuidv1()}>
-    //         <img src={logo} alt="" className="Logo-responsive" />
-    //       </div>
-    //     ))}
-    //   </Slider>
-    // );
-    const settings = {
-      pauseOnHover: false,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      speed: 4000,
-      dots: false,
-      autoplaySpeed: 0,
-      cssEase: 'linear',
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true,
-          },
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2,
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },
-      ],
-    };
+    const { logos } = this.state;
     return (
       <Grid item xs={8}>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
+        <Slider {...carouselSettings}>
+          {logos.map(logo => (
+            <div key={uuidv1()}>
+              <img src={logo} alt="" className="Logo-responsive" />
+            </div>
+          ))}
         </Slider>
       </Grid>
     );
