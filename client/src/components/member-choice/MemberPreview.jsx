955a2415fc7134a9f5f32fb9aa2790abc478374f
Ben Hubsch <bah37@duke.edu>
2018-11-21 13:08:19 -0500

    Pushing font changes so Jeremy can test.

--- client/src/components/member-choice/MemberPreview.jsx
+++ client/src/components/member-choice/MemberPreview.jsx
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 import '../../css/member-choice/MemberPreview.css';
 
 const srcImage = 'https://image.ibb.co/bQBKnA/24902379.png';
-const name = 'Ben';
+const name = 'Ben Hubsch';
 
 export default class Companies extends Component {
   render() {
-    return <img src={srcImage} alt={name} className="MemberPreview-picture" />;
+    return (
+      <>
+        <img src={srcImage} alt={name} className="MemberPreview-picture" />
+        <div className="MemberPreview-name">{name}</div>
+      </>
+    );
   }
 }
