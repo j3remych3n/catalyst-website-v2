cefc7c75f5a4ba6d540768138cb1db0d20429a14
Bao Van <duybaovan@gmail.com>
2018-11-21 15:03:38 -0500

    added Grids and stuff

---
+++ client/src/components/Section.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types"; // ES6
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+export default class Section extends Component {
+  render() {
+    return (
+      <Grid
+        className="section"
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        //left
+        <Grid
+          style={{ minHeight: "100%", backgroundColor: "white" }}
+          item
+          xs={3}
+        >
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          />
+        </Grid>
+        //right
+        <Grid item xs={9}>
+          <Grid container>
+            <Paper style={{ minWidth: "100%", minHeight: "100%" }}> aaa </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
