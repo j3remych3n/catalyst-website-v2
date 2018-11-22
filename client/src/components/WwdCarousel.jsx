bb3c3e3428cf2cab09bdb33320e889e8e73026c1
Austinn Carter <asc63@duke.edu>
2018-11-21 19:51:52 -0500

    Created a carousel for group pics, added api calls for group pics

---
+++ client/src/components/WwdCarousel.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import uuidv1 from 'uuid/v1';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import '../css/WwdCarousel.css';
+
+const carouselSettings = {
+  pauseOnHover: false,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  dots: false,
+  speed: 1000,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+};
+
+const styles = {
+  imgSize: {
+    maxWidth: 200,
+  },
+};
+
+export default class WwdCarousel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pics: [],
+    };
+
+    axios
+      .get('/api/grouppictures')
+      .then(response => this.setState({ pics: response.data.pictures }))
+      .catch(() => {});
+  }
+
+  render() {
+    const { pics } = this.state;
+    return (
+      <Grid item style={styles.imgSize}>
+        <Slider {...carouselSettings}>
+          {pics.map(pic => (
+            <div key={uuidv1()}>
+              <img src={pic} alt="" className="Logo-responsive" />
+            </div>
+          ))}
+        </Slider>
+      </Grid>
+    );
+  }
+}
