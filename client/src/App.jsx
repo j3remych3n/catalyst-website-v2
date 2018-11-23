5c5305394fee3dd352a5a16fac2d5832ae4ca0c5
Dorian Barber <dpb20@duke.edu>
2018-11-23 18:37:14 -0500

    grabbed app.jsx from master

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Companies from "./components/Companies";
 import Home from "./components/Home";
-import CompanyCarousel from "./components/CompanyCarousel";
 
 import logo from "./logo.svg";
 import "./css/App.css";
@@ -11,22 +10,32 @@ import "./css/App.css";
 import "./fonts.css";
 import WwdCarousel from "./components/WwdCarousel";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">
-        {" "}
-        Tooday is the best day to take too many Ls.{" "}
-      </h1>
-    </header>
-    <Grid container alignItems="center" justify="center">
-      <Companies />
-    </Grid>
-    <Grid xs={8} item style={{ backgroundColor: "pink" }}>
-      <CompanyCarousel />
-    </Grid>
-  </div>
-);
+// const App = () => (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <h1 className="App-title">ok</h1>
+//     </header>
+//     <Grid container alignItems="center" justify="center">
+//       <Companies />
+//     </Grid>
+//     <Grid container alignItems="center" justify="center">
+//       <WwdCarousel />
+//     </Grid>
+//   </div>
+// );
+// const App = () => (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <h1 className="App-title">Hello World</h1>
+//     </header>
+//     <Grid container alignItems="center" justify="center">
+//       <Companies />
+//     </Grid>
+//   </div>
+// );
+
+const App = () => <Home />;
 
 export default App;
