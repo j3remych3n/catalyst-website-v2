fb7bc8fe721e330c9bf6cd5586882eff36c4b163
Austinn Carter <asc63@duke.edu>
2018-11-23 16:31:22 -0500

    finished faq component

--- client/src/components/Faq.jsx
+++ client/src/components/Faq.jsx
@@ -1,5 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
+import uuidv1 from "uuid/v1";
+import colors from "../colors.js";
+
+const styles = {
+  qTag: {
+    color: colors.pink,
+    display: "inline"
+  },
+  aTag: {
+    color: colors.yellow,
+    display: "inline"
+  },
+  txtStyle: {
+    color: colors.white,
+    display: "inline"
+  }
+};
 
 export default class Faq extends Component {
   constructor(props) {
@@ -17,7 +34,28 @@ export default class Faq extends Component {
 
   render() {
     const { faq } = this.state;
-    console.log(faq);
-    return <h1> HELLO</h1>;
+    const qTagOpen = "<q>";
+    const qTagClose = "</q>";
+    const aTagOpen = "<a>";
+    const aTagClose = "</a>";
+
+    return (
+      <div align="left">
+        {faq.map(pair => (
+          <div key={uuidv1()}>
+            <div>
+              <div style={styles.qTag}>{qTagOpen}</div>
+              <div style={styles.txtStyle}>{pair.Key}</div>
+              <div style={styles.qTag}>{qTagClose}</div>
+            </div>
+            <div>
+              <div style={styles.aTag}>{aTagOpen}</div>
+              <div style={styles.txtStyle}>{pair.Value}</div>
+              <div style={styles.aTag}>{aTagClose}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
